644116aa5616260aedd6e5d9452a036b13d2235f
David Day <david.day@utexas.edu>
2022-08-25 21:36:40 +0000

    add omz convenience aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -3,3 +3,6 @@ alias vimconfig='vim ~/.vimrc'
 alias zshconfig='vim ~/.zshrc' 
 alias i3config='vim ~/.i3/config'
 alias cs='config status'
+alias omzupdate='git -C ~/.oh-my-zsh pull --rebase origin master'
+alias omzupdate-personal='git -C ~/.oh-my-zsh pull --rebase personal main'
+
